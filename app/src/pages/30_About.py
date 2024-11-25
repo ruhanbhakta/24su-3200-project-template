09192540c69ad8fac329ab9dd94126a9dcbac3e6
ruhanbhakta <ruhanbhakta@gmail.com>
2024-11-25 11:12:12 -0500

    Small updates to home and about pages- testing streamlit out

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -8,11 +8,9 @@ st.write("# About this App")
 
 st.markdown (
     """
-    This is a demo app for CS 3200 Course Project.  
+    Picture this: you’re a college student, anxious and unsure as you navigate your first co-op search with no experience and little guidance. Or maybe you’re a recruiter, spending countless hours sorting through unqualified candidates and struggling to find the right fit for your growing company. And then there’s the co-op advisor—overwhelmed, managing hundreds of students, each needing unique support, with no efficient way to keep up.
+    Enter FreshMeet, the data-driven platform poised to revolutionize how co-ops are experienced. This app tackles the biggest pain points in the current system: the disorganized, stressful hunt for the right opportunities, inefficient hiring processes, and the struggle for personalized guidance in a sea of students. With FreshMeet, students get personalized dashboards that track their applications and skill progress, making the job search feel less like a maze and more like a clear path to success. Recruiters can sort and filter candidates effortlessly, accessing skill-based matches and real-time updates. And for advisors, FreshMeet provides an intuitive interface to monitor, support, and manage students’ co-op journeys at scale.
+    Built for students seeking their first chance, recruiters who value efficiency, and advisors who need a powerful yet simple management tool, FreshMeet is here to elevate the co-op experience from frustration to seamless connection. FreshMeet is your next big investment. FreshMeet brings desperately needed order and data insights to the Northeastern co-op experience.
 
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
-
-    Stay tuned for more information and features to come!
     """
         )
