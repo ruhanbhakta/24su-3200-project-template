708e517b7205ee7bc6de17423fe45db89bba7404
yuan476 <150469923+yuan476@users.noreply.github.com>
2024-12-03 09:59:51 -0500

    Added a GET route for grabbing jobs for the student that match their skills.

--- api/backend/students/student_routes.py
+++ api/backend/students/student_routes.py
@@ -41,3 +41,45 @@ def student_job_postings():
         # Log the error and return a response
         current_app.logger.error(f"Error fetching jobs: {e}")
         return jsonify({"error": "Failed to fetch jobs"}), 500
+    
+# Grab job postings that matches the student's skills.
+@student.route('/matching_job_postings', methods=['GET'])
+def student_matching_postings():
+    query = '''
+        SELECT DISTINCT
+        j.jobId,
+        j.title,
+        ps.skillId,
+        s.name AS SkillName,
+        ps.expectedProficiency
+        FROM
+        JobPosting j
+        JOIN
+        PostingSkills ps ON j.jobId = ps.jobId
+        JOIN
+        StudentSkills ss ON ps.skillId = ss.skillId
+        JOIN
+        Skills s ON ps.skillId = s.skillId
+        WHERE
+        ss.studentId = 1
+        AND ss.proficiency >= ps.expectedProficiency;
+        '''
+    try:
+        # Get a database connection
+        connection = db.connect()
+        cursor = connection.cursor()
+
+        # Execute query
+        cursor.execute(query)
+        student_matching_postings = cursor.fetchall()
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+        return jsonify(student_matching_postings), 200
+
+    except Exception as e:
+        # Log the error and return a response
+        current_app.logger.error(f"Error fetching matching jobs: {e}")
+        return jsonify({"error": "Failed to fetch matching jobs"}), 500
